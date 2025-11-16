fad406b4150fa1a48d15e44a61f67c26fc98acaf
Oussama Bahya <obahya@e2r6p6.1337.ma>
2025-11-16 21:21:16 +0100

    C00

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obahya <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 17:35:55 by obahya            #+#    #+#             */
+/*   Updated: 2025/07/16 19:26:19 by obahya           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print_nb(int a, int b)
+{
+	ft_putchar((a / 10) + '0');
+	ft_putchar((a % 10) + '0');
+	ft_putchar(' ');
+	ft_putchar((b / 10) + '0');
+	ft_putchar((b % 10) + '0');
+	if (a < 98)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			print_nb(a, b);
+			b++;
+		}
+		a++;
+	}
+}
