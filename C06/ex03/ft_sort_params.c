5f0f32ed8549f5a0278c9b8e30a353df5381853a
Oussama Bahya <obahya@e2r6p6.1337.ma>
2025-11-16 21:39:43 +0100

    C06

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obahya <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/28 21:03:40 by obahya            #+#    #+#             */
+/*   Updated: 2025/07/28 21:03:44 by obahya           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}	
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_swap(char **s1, char **s2)
+{
+	char	*temp;
+
+	temp = *s1;
+	*s1 = *s2;
+	*s2 = temp;
+}
+
+int	main(int ac, char **av)
+{
+	int		i;
+	int		j;
+
+	i = 1;
+	while (i < ac)
+	{
+		j = i + 1;
+		while (j < ac)
+		{
+			if (ft_strcmp(av[i], av[j]) > 0)
+				ft_swap(&av[i], &av[j]);
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (av[i])
+	{
+		ft_putstr(av[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
